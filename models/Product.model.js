41486efca44c7ae31b2faade933dec4df52bb19c
aslanlisuleyman <tu7fzl01d.code.edu.az>
2024-01-07 23:43:55 +0400

    creatt

--- models/Product.model.js
+++ models/Product.model.js
@@ -1,6 +1,6 @@
 const mongoose =require ("mongoose")
 
-const Product = mongoose.model("Product", new mongoose.Schema({
+const Product = mongoose.model("Products", new mongoose.Schema({
 
     title: String,
     price: Number,
